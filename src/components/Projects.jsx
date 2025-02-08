08fbf9541fb7b9a7405d917928df693f98f8b87c
stack-shailendra <stack.shailendra@gmail.com>
2025-02-08 17:28:06 +0530

    added image link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 
 
-import { div, h1, link } from "framer-motion/client"
+import { div, h1, image, link } from "framer-motion/client"
 import portfolio from "../assets/portfolio.png"
 import weather from "../assets/weather.png"
 import moen from "../assets/moen.png"
@@ -15,15 +15,15 @@ const projectsData = [
     link: "https://fitclubzym.pages.dev/",
     title: "FitClub",
     description: "Developed and maintained the FitClub web application using HTML, CSS, JavaScript, and React. This is fully responsive single page application.",
-   
-    technologies: ["HTML", "CSS", "JavaScript","React"],
+
+    technologies: ["HTML", "CSS", "JavaScript", "React"],
 
   },
   {
     image: portfolio,
     link: "https://myportfolio-1h6.pages.dev/",
     title: "My Portfolio",
-    description: "Developed and maintained the Portfolio web application using HTML, TailwindCSS, JavaScript, and React. This is fully responsive single page application, in this project there is a contact service by using this you can directly contact with me.",
+    description: "Developed and maintained the Portfolio web application using HTML, TailwindCSS, JavaScript, and React. This is fully responsive single page application, in this project there is a contact service by using this you can directly contact with me.     ",
     technologies: ["HTML", "TailwindCSS", "JavaScript", "React"],
 
   },
@@ -32,7 +32,7 @@ const projectsData = [
     link: "https://test.socialannex.com/moen/0/loyalty/dashboard.php?email=john.sa@gmail.com",
     title: "Club Moen",
     description: "Design Ecommerce website using HTML, CSS, JavaScript, and jQuery. In this project user can earn money by completing tasks and earn actions like purchase,birthday,facebook connect etc.",
-    technologies: ["HTML", "CSS", "JavaScript",'jQuery'],
+    technologies: ["HTML", "CSS", "JavaScript", 'jQuery'],
 
   },
   {
@@ -45,27 +45,35 @@ const projectsData = [
   }
 ]
 
-const ScrollReveal = ({children})=>{
-  return(
+const ScrollReveal = ({ children }) => {
+  return (
     <motion.div
-    initial={{opacity:0,y:100}}
-    whileInView={{opacity:1,y:0}}
-    viewport={{once:true}}
-    transition={{duration:0.8}}
-    
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8 }}
+
     >
       {children}
     </motion.div>
   )
 }
 
+
+
 const ProjectCard = ({ project }) => {
   return (
     <ScrollReveal>
-    <div className="flex flex-col items-center gap-8 md:flex-row md:gap-24">
-     <a href={project.link} target="_blank"> <img src={project.image} alt="" className="w-full cursor-pointer
+       <div className="flex flex-col items-center gap-6 md:flex-row md:gap-24">
+     
+      
+        <img
+        src={project.image} alt="" className="w-[500px] cursor-pointer
       rounded-2xl transition-all duration-300
-      hover:scale-105 md:w-[300px]" /></a> 
+      hover:scale-105 md:w-[300px]"   
+        onClick={() => window.open(`${project.link}`, "_blank")}
+        />
+    
 
       <div className="flex flex-col gap-3 ">
         <div className="text-xl font-semibold">{project.title}</div>
@@ -83,6 +91,7 @@ const ProjectCard = ({ project }) => {
     
 
     </div>
+
     </ScrollReveal>
   )
 }
@@ -93,14 +102,14 @@ const Projects = () => {
       <div id="projects" className="flex min-h-screen w-full flex-col items-center justify-center
            gap-16 p-4 md:px-14 md:py-24 ">
         <ScrollReveal>
-        <h1 className="text-4xl font-light text-white md:text-6xl">
-          My Projects
-        </h1>
+          <h1 className="text-4xl font-light text-white md:text-6xl">
+            My Projects
+          </h1>
         </ScrollReveal>
         <div className="flex w-full max-w-[1000px] flex-col gap-16 text-white"                                >
           {
             projectsData.map((project, index) => (
-              <ProjectCard  key = {index} project = {project} />
+              <ProjectCard key={index} project={project} />
             ))
 
           }
